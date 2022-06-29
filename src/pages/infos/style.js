30e239713f6c6a44e8b92d92bc40e02fbfd921d7
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-06-29 18:04:20 -0300

    style: style for school students page

--- src/pages/infos/style.js
+++ src/pages/infos/style.js
@@ -4,6 +4,8 @@ export const Content = styled.div`
     display:flex;
     justify-content:center;
     margin:5rem 0;
+    position:relative;
+    z-index:0;
 `
 
 export const Houses = styled.img`
@@ -26,10 +28,64 @@ export const Houses = styled.img`
 `
 
 export const Modals = styled.div`
-    // background:red;
     color:white;
-    // height:100vh;
     display:flex;
     justify-content:center;
 
 `
+
+export const Modal = styled.div`
+    background: rgb(149,125,58);
+    background: linear-gradient(90deg, rgba(149,125,58,1) 0%, rgba(231,185,115,1) 44%);
+    color:white;
+    height:90vh;
+    display:flex;
+    justify-content:center;
+    position:absolute;
+    z-index:99999999999;
+    top:5vh;
+    bottom:20px;
+    width:90vw;
+    border-radius:20px;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+`
+
+export const Cancel = styled.div`
+    width:2vw;
+    position:absolute;
+    z-index:999999999;
+    background:#ffc107;
+    color:#212529;
+    right:-20px;
+    top:-15px;
+    text-align:center;
+    border:2px solid white;
+    border-radius:1vw;
+    padding:10px;
+    cursor:pointer;
+`
+
+export const DataContent = styled.div`
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    width:90vw;
+    flex-wrap:wrap;
+`;
+
+export const Alunos = styled.div`
+    margin:0px 10px;
+    padding:0 5px;
+    border-bottom:2px solid #FF8000;
+    font-size:1.25rem;
+    font-weight:600;
+    cursor:pointer;
+    color:#5A5151;
+
+    &:hover{
+        box-shadow: -5px 5px 20px #FF8000;
+        transition:0.55s;
+        border-radius:1vw;
+    }
+`;  
+
